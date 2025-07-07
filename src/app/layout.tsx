70d9e506ac412fb7f799a4677f3c2780a55c238f
hongryeollim <hongryeollim@lscompany.co.kr>
2025-07-07 09:35:46 +0900

    metadata 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Our Story",
-  description: "Our Story Signup",
+  title: "우리의 이야기",
+  description:
+    "함께 써 내려가는 이야기, 지금 Our Story에 가입하고 당신의 이야기를 남겨보세요.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
